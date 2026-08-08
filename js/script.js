fc71512dd1493c285ef1635e23b75bda447b39f2
Bekhzod Istamov <isbek2000@gmail.com>
2026-08-08 16:41:03 +0500

    I started working with a timer, wrote a function that gets the difference between the deadline and today, and parses it by day, hour, minute, and second.

--- js/script.js
+++ js/script.js
@@ -35,4 +35,18 @@ window.addEventListener('DOMContentLoaded', () => {
 			});
 		}
 	});
+
+	//Timer
+
+	const deadline = '2026-08-28';
+
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			seconds = Math.floor((t / 1000) % 60);
+		console.log(days, hours, minutes, seconds);
+	}
+	getTimeRemaining(deadline);
 });
