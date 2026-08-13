4c1baef1320a912cb1022fac95279c64c54c399f
Bekhzod Istamov <isbek2000@gmail.com>
2026-08-13 20:46:59 +0500

    finished working on the classes

--- js/script.js
+++ js/script.js
@@ -139,7 +139,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	window.addEventListener('scroll', showModalByScroll);
 
-	// const modalTimerId = setTimeout(openModal, 10000);
+	const modalTimerId = setTimeout(openModal, 10000);
 
 	//Class
 
@@ -151,7 +151,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			this.descr = descr;
 			this.price = price;
 			this.parent = document.querySelector(parentSelector);
-			this.transfer = 27;
+			this.transfer = 25;
 			this.changeToUAH();
 		}
 
@@ -163,20 +163,42 @@ window.addEventListener('DOMContentLoaded', () => {
 			const element = document.createElement('div');
 
 			element.innerHTML = `
-               <div class="menu__item">
-						<img src="img/tabs/vegy.jpg" alt="vegy" />
-						<h3 class="menu__item-subtitle">Меню "Фитнес"</h3>
-						<div class="menu__item-descr">
-							Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и
-							здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!
-						</div>
-						<div class="menu__item-divider"></div>
-						<div class="menu__item-price">
-							<div class="menu__item-cost">Цена:</div>
-							<div class="menu__item-total"><span>229</span> грн/день</div>
-						</div>
-					</div>
+            <div class="menu__item">
+               <img src=${this.src} alt=${this.alt} />
+               <h3 class="menu__item-subtitle">${this.title}</h3>
+               <div class="menu__item-descr">${this.descr}</div>
+               <div class="menu__item-divider"></div>
+               <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+               </div>
+            </div>
          `;
+			this.parent.append(element);
 		}
 	}
+	new MenuCard(
+		'img/tabs/vegy.jpg',
+		'vegy',
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+		'9',
+		'.menu .container'
+	).render();
+	new MenuCard(
+		'img/tabs/elite.jpg',
+		'elite',
+		'Меню “Премиум”',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		'15',
+		'.menu .container'
+	).render();
+	new MenuCard(
+		'img/tabs/post.jpg',
+		'post',
+		'Меню "Постное"',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		'9',
+		'.menu .container'
+	).render();
 });
