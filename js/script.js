1299fa7b06b2a34ec6e1374d619a2478c209fd27
Bekhzod Istamov <isbek2000@gmail.com>
2026-08-13 20:08:55 +0500

    We continue to work on classes, I am already creating a render method that will place the template on the page

--- js/script.js
+++ js/script.js
@@ -144,17 +144,39 @@ window.addEventListener('DOMContentLoaded', () => {
 	//Class
 
 	class MenuCard {
-		constructor(src, alt, title, descr, price) {
+		constructor(src, alt, title, descr, price, parentSelector) {
 			this.src = src;
 			this.alt = alt;
 			this.title = title;
 			this.descr = descr;
 			this.price = price;
+			this.parent = document.querySelector(parentSelector);
 			this.transfer = 27;
+			this.changeToUAH();
 		}
 
 		changeToUAH() {
-			this.transfer * this.price;
+			this.price = this.transfer * this.price;
+		}
+
+		render() {
+			const element = document.createElement('div');
+
+			element.innerHTML = `
+               <div class="menu__item">
+						<img src="img/tabs/vegy.jpg" alt="vegy" />
+						<h3 class="menu__item-subtitle">Меню "Фитнес"</h3>
+						<div class="menu__item-descr">
+							Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и
+							здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!
+						</div>
+						<div class="menu__item-divider"></div>
+						<div class="menu__item-price">
+							<div class="menu__item-cost">Цена:</div>
+							<div class="menu__item-total"><span>229</span> грн/день</div>
+						</div>
+					</div>
+         `;
 		}
 	}
 });
