8c648b20ecbe8946e06e0cfa8960ff5d32f71226
Bekhzod Istamov <isbek2000@gmail.com>
2026-08-26 21:19:58 +0500

    data sending is working successfully

--- js/script.js
+++ js/script.js
@@ -139,7 +139,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	window.addEventListener('scroll', showModalByScroll);
 
-	//const modalTimerId = setTimeout(openModal, 10000);
+	const modalTimerId = setTimeout(openModal, 10000);
 
 	//Class
 
@@ -209,12 +209,45 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Forms
 	const forms = document.querySelectorAll('form');
 
+	const message = {
+		loading: 'Загрузка',
+		success: 'Спасибо! Скоро с вами свяжемся',
+		failure: 'Что-то пошло не так...',
+	};
+
+	forms.forEach(item => {
+		postData(item);
+	});
+
 	function postData(form) {
 		form.addEventListener('submit', e => {
 			e.preventDefault();
 
+			const statusMessage = document.createElement('div');
+			statusMessage.classList.add('status');
+			statusMessage.textContent = message.loading;
+			form.append(statusMessage);
+
 			const request = new XMLHttpRequest();
 			request.open('POST', 'server.php');
+
+			//request.setRequestHeader('Content-type', 'multipart/form-data');
+			const formData = new FormData(form);
+
+			request.send(formData);
+
+			request.addEventListener('load', () => {
+				if (request.status === 200) {
+					console.log(request.response);
+					statusMessage.textContent = message.success;
+					form.reset();
+					setTimeout(() => {
+						statusMessage.remove();
+					}, 2000);
+				} else {
+					statusMessage.textContent = message.failure;
+				}
+			});
 		});
 	}
 });
